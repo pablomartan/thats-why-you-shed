8f4b0526e63d1800bbaeaf70fb1cb2fd79e83252
Pablo Martínez <pablomartan@kolabnow.com>
2025-10-24 16:31:37 +0200

    feat: add time signature selector

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import MetronomeAudio from "@/audio-context";
-import Metronome from "@/metronome";
+import Metronome, { TimeSignature } from "@/metronome";
 
 let audioContext: MetronomeAudio;
 let metronome: Metronome;
 
-const init = () => {
-  if (metronome) {
-    return;
-  }
-
+const init = (timeSig: TimeSignature) => {
   const bpmInput = Array.from(document.getElementsByTagName("input"))[0];
   const bpm = (60 / Number(bpmInput.value)) * 1000;
 
   audioContext = new MetronomeAudio();
   metronome = new Metronome({
     bpm,
-    timeSig: "4/4",
+    timeSig,
     muteBars: 0,
     tickFunction: audioContext.tickSound,
     stopFunction: audioContext.stop,
@@ -25,9 +21,13 @@ const init = () => {
 document.addEventListener("DOMContentLoaded", () => {
   const playButton = document.querySelector(".play-button");
   const stopButton = document.querySelector(".stop-button");
+  const numerator = document.querySelector(".numerator") as HTMLSelectElement;
+  const denominator = document.querySelector(
+    ".denominator",
+  ) as HTMLSelectElement;
 
   playButton.addEventListener("click", () => {
-    init();
+    init(`${numerator.value}/${denominator.value}` as TimeSignature);
     metronome.startMetronome();
   });
 
